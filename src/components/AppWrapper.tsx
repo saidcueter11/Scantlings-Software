39b4234d15393c1e6b54bb7cc47b9377b1760f42
Said Cueter <rattion@pop-os.localdomain>
2023-11-28 06:57:59 -0500

    Fixing image deploy

--- src/components/AppWrapper.tsx
+++ src/components/AppWrapper.tsx
@@ -4,7 +4,7 @@ export const AppWrapper = ({ children }: { children: React.ReactNode }) => {
   return (
     <main className='w-full overflow-x-hidden overflow-y-scroll h-screen '>
       <nav className='w-full sticky top-0 backdrop-blur-sm'>
-        <img className='h-16 ml-8 mt-3' src="/src/assets/LOGOTIPO ECOTEA-06.png" alt="" />
+        <img className='h-16 ml-8 mt-3' src="../../src/assets/LOGOTIPO ECOTEA-06.png" alt="" />
       </nav>
       <main className="flex justify-center items-center flex-col gap-4">
         <h1 className='mb-2 text-3xl font-bold'>Scantlings Software</h1>
